7afc386411c824e1c0a74ece4074d53d9ba0591d
DeshChow <deshchow001@gmail.com>
2022-09-06 17:58:48 +0600

    third-commit

--- projects/views.py
+++ projects/views.py
@@ -1,24 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Project
-
-projectsList = [
-    {
-        'id': '1',
-        'title': 'Ecommerce Website',
-        'description': 'Fully functional ecommerce website'
-    },
-    {
-        'id': '2',
-        'title': 'Portfolio Website',
-        'description': 'A personal website to write articles and display work'
-    },
-    {
-        'id': '3',
-        'title': 'Social Network',
-        'description': 'An open source project built by the community'
-    }
-]
+from  .forms import ProjectForm
 
 def projects(request):
     projectsList = Project.objects.all()
@@ -31,5 +14,33 @@ def project(request, pk):
     return render(request, 'projects/single-project.html', {'project': projectObj, 'tags' : tags})
 
 def createProject(request):
-    context = {}
+    form = ProjectForm()
+    if request.method == 'POST':
+        form = ProjectForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('projects') 
+
+    context = {'form': form}
     return render(request, 'projects/project_form.html', context)
+
+def updateProject(request, pk):
+    project = Project.objects.get(id=pk)
+    form = ProjectForm(instance=project)
+    if request.method == 'POST':
+        form = ProjectForm(request.POST, request.FILES, instance=project)
+        if form.is_valid():
+            form.save()
+            return redirect('projects') 
+
+    context = {'form': form}
+    return render(request, 'projects/project_form.html', context)
+
+def deleteProject(request, pk):
+    project = Project.objects.get(id=pk)
+    if request.method == 'POST':
+        project.delete()
+        return redirect('projects')
+
+    context = {'object': project}
+    return render(request, 'projects/delete_template.html', context)
