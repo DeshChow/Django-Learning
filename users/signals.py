45d86148b738b2858d5f318c0a9aafca215008d7
DeshChow <deshchow001@gmail.com>
2022-09-09 16:30:14 +0600

    Authentication & Authorization done

--- users/signals.py
+++ users/signals.py
@@ -4,7 +4,6 @@ from django.dispatch import receiver
 from django.contrib.auth.models import User
 from .models import Profile
 
-
 # signals
 # @receiver(post_save, sender=Profile)
 def createProfile(sender, instance, created, **kwargs):
