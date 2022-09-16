55e79ee144191787d3302640b1eca2b3a365580f
DeshChow <deshchow001@gmail.com>
2022-09-16 17:54:16 +0600

    Reviews & Messages

--- projects/views.py
+++ projects/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render, redirect
 # from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from .models import Project
-from  .forms import ProjectForm
+from  .forms import ProjectForm, ReviewForm
+from django.contrib import messages
 from .utils import searchProjects, paginateProjects
 
 def projects(request):
@@ -15,8 +16,22 @@ def projects(request):
 
 def project(request, pk):
     projectObj = Project.objects.get(id=pk)
-    tags = projectObj.tags.all()
-    return render(request, 'projects/single-project.html', {'project': projectObj, 'tags' : tags})
+    form = ReviewForm()
+
+    if request.method == 'POST':
+        form = ReviewForm(request.POST)
+        review = form.save(commit=False)
+        review.project = projectObj
+        review.owner = request.user.profile
+        review.save()
+
+        projectObj.getVoteCount
+
+        messages.success(request, 'Your review was successfully submitted!')
+        return redirect('project', pk=projectObj.id)
+
+    return render(request, 'projects/single-project.html', {'project': projectObj, 'form': form})
+
 
 @login_required(login_url="login")
 def createProject(request):
